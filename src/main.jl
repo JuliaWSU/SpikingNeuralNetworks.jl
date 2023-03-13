8c307a37f83040b585ef00c829b8633cd3d81d64
Russell Jarvis <R.jarvis@westernsydney.edu.au>
2023-03-13 12:44:14 +1100

    update 16bit float to 32bit

--- src/main.jl
+++ src/main.jl
@@ -1,6 +1,14 @@
-function sim!(P, C, dt)
-    Threads.@threads for p in P
-        integrate!(p, p.param, Float32(dt))
+
+
+function sim!(P, C; dt = 0.1ms, duration = 10ms)
+    for t = 0ms:dt:(duration - dt)
+        sim!(P, C, dt)
+    end
+end
+
+function sim16!(P, C, dt::Float16)
+    for p in P
+        integrate!(p, p.param, Float16(dt))
         record!(p)
     end
     for c in C
@@ -9,11 +17,45 @@ function sim!(P, C, dt)
     end
 end
 
-function sim!(P, C; dt = 0.1ms, duration = 10ms)
-    for t = 0ms:dt:(duration - dt)
-        sim!(P, C, dt)
+function sim!(P, C, dt::Float64)
+    for p in P
+        #integrate!(::SpikingNeuralNetworks.IF{Vector{Float32}, Vector{Bool}}, ::SpikingNeuralNetworks.IFParameter, ::Float16)
+
+        integrate!(p, p.param, dt)
+        record!(p)
+    end
+    for c in C
+        forward!(c, c.param)
+        record!(c)
     end
 end
+
+function sim16!(P, C; dt::Float64 = 0.1ms, duration::Float64 = 10ms)
+    for t = 0ms:dt:(duration - dt)
+        sim!(P, C, Float16(dt))
+    end
+end
+
+
+
+function train16!(P, C, dt::Float16, t = 0.0)
+    for p in P
+        integrate!(p, p.param, Float16(dt))
+        record!(p)
+    end
+    for c in C
+        forward!(c, c.param)
+        plasticity!(c, c.param, Float16(dt), Float32(t))
+        record!(c)
+    end
+end
+
+function train16!(P, C; dt = 0.1ms, duration = 10ms)
+    for t = 0ms:dt:(duration - dt)
+        train!(P, C, Float16(dt), t)
+    end
+end
+
 
 function train!(P, C, dt, t = 0)
     for p in P
--- src/neuron/if.jl
+++ src/neuron/if.jl
@@ -1,63 +1,31 @@
-#valgrind --smc-check=all-non-file julia if_net.jl
 
-#@snn_kw 
 abstract type AbstractIFParameter end
 
-struct IFParameter <: AbstractIFParameter #<: AbstractIFParameter
-    τm::Float32 # = 20#ms
-    τe::Float32 # = 5#ms
-    τi::Float32 # = 10#ms
-    Vt::Float32 # = -50#mV
-    Vr::Float32 # = -60#mV
-    El::Float32 # = -65
+struct IFParameter <: AbstractIFParameter 
+    τm::Float32 
+    τe::Float32 
+    τi::Float32 
+    Vt::Float32 
+    Vr::Float32 
+    El::Float32 
 end
 
-    #function IFParameter{Vector{Float32}}(τm::Float32,τe::Float32,τi::Float32,Vt::Float32,Vr::Float32,El::Float32)# where T<:Integer
-    #    new(τm, τe, τi, Vt, Vr, El)
-    #end
-    #function IFParameter()
-        #(τm::Float32,τe::Float32,τi::Float32,Vt::Float32,Vr::Float32,El::Float32)# where T<:Integer
-    #    new(20.0, 5.0, 10.0, -50.0, -60.0, -49.0)
-    #end
-
-    #IFParameter{T<:Float32}  = new(20.0,5.0,10.0,-50.0,-60.0,-65.0)
-function IFParameter()#;τm::Float32,τe::Float32,τi::Float32,Vt::Float32,Vr::Float32,El::Float32)#20.0,5.0,10.0,-50.0,-60.0,-65.0)
-    FT = Float32
-    τm::FT = 20ms
-    τe::FT = 5ms
-    τi::FT = 10ms
-    Vt::FT = -50mV
-    Vr::FT = -60mV
-    El::FT = -65.0
-    return IFParameter(20ms,5.0,10.0,-50.0,-60.0,-65.0)
-end    
-
-
-#IFParameter = IFParameter()
-
-#=
 function IFParameter()
-    FT=Float32
-    τm::FT = 20ms
-    τe::FT = 5ms
-    τi::FT = 10ms
-    Vt::FT = -50mV
-    Vr::FT = -60mV
-    El::FT = Vr
+    return IFParameter(20,5.0,10.0,-50.0,-60.0,-49)
 end    
-IFParameter = IFParameter()
-=#
+function IFParameter(;τm::Float32=20ms,τe::Float32=5.0,τi::Float32=10.0,Vt::Float32=-50.0,Vr::Float32=-60.0,El::Float32=-49mV)
+    return IFParameter(τm,τe,τi,Vt,Vr,El)
+end
 abstract type AbstractIF end
 
 @snn_kw mutable struct IF{VFT=Vector{Float32},VBT=Vector{Bool}} <: AbstractIF
     param::IFParameter = IFParameter()
-    #@show(param)
-    N::Int32 = 100
-    v::VFT = ones(N).*param.Vr #.+ rand(N) .* (param.Vt - param.Vr)
-    ge::VFT = zeros(N)
-    gi::VFT = zeros(N)
+    N::Int32 = 10
+    v::Vector{Float32} = param.Vr .+ rand(N) .* (param.Vt - param.Vr)
+    ge::Vector{Float32} = zeros(N)
+    gi::Vector{Float32} = zeros(N)
     fire::VBT = zeros(Bool, N)
-    I::VFT = zeros(N)
+    I::Vector{Float32} = zeros(N)
     records::Dict = Dict()
 end
 
@@ -66,6 +34,27 @@ end
 """
 IF
 
+#integrate!(::SpikingNeuralNetworks.IF{Vector{Float32}, Vector{Bool}}, ::SpikingNeuralNetworks.IFParameter, ::Float16)
+
+function integrate!(p::IF, param::IFParameter, dt::Float64)
+    """
+    Integrating over cells, not integrating over time. 
+    An ideal place for parallelisation.
+    """
+
+    @unpack N, v, ge, gi, fire, I = p
+    @unpack τm, τe, τi, Vt, Vr, El = param
+    @inbounds for i = 1:N
+        v[i] += dt * (ge[i] + gi[i] - (v[i] - El) + I[i]) / τm
+        ge[i] += dt * -ge[i] / τe
+        gi[i] += dt * -gi[i] / τi
+    end
+    @inbounds for i = 1:N
+        fire[i] = v[i] > Vt
+        v[i] = ifelse(fire[i], Vr, v[i])
+    end
+end
+#integrate!(p::IF, param::IFParameter, dt::Float64) = integrate!(p::IF, param::IFParameter, dt::Float32)
 function integrate!(p::IF, param::IFParameter, dt::Float32)
     @unpack N, v, ge, gi, fire, I = p
     @unpack τm, τe, τi, Vt, Vr, El = param
